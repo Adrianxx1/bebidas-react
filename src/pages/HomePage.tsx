23741177ecb33fb99ce9262e8d8a925dac9357b0
Adrianxx1 <triaxis12q@gmail.com>
2025-02-18 13:04:36 -0600

    comit 09

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,23 @@
+import { useMemo } from "react"
+import { useAppStore } from "../stores/useAppStore"
 export default function HomePage(){
+    const recipes = useAppStore((state) => state.recipes)
+
+
+    const  hasRecipes = useMemo (() => recipes.drinks.length > 0 , [recipes])
     return (
         <>
-        <h1>Home Page</h1>
+        <h1 className="text-6xl front-extrabold">Recetas</h1>
+       
+
+        {hasRecipes ? (
+            <>
+            <p>Si hay bebidas</p>
+            </> ):(
+                <p className="my-10 text center text-2xl">No hay bebidas, busca en el formulario</p>
+            )
+        }
         </>
     )
+    
 }
